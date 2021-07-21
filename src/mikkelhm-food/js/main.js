d27b0d9c76b1e9c17605bce2ecce75a71a7203e8
mikkelhm <mikkel@h-madsen.dk>
2021-07-21 22:01:32 +0200

    Update things

--- src/mikkelhm-food/js/main.js
+++ src/mikkelhm-food/js/main.js
@@ -4,22 +4,25 @@ document.addEventListener('alpine:init', () => {
     Alpine.store('recipieData', {
         currentRecipie: null,
         recipies: [],
+        navOpen: true,
+        toggleNav() {
+            this.navOpen = !this.navOpen;
+        },
         init() {
-            console.log("now");
             fetch(allRecipies, {
                 headers: {
                     'Accept-Language': 'da-DK',
                     'umb-project-alias': 'mikkelhm-food'
                 }
             })
-            .then((response) => {
-                return response.json();
-            })
-            .then((myJson) => {
-                console.log(myJson._embedded.content);
-                this.recipies = myJson._embedded.content;
-                onRouteChanged();
-            });
+                .then((response) => {
+                    return response.json();
+                })
+                .then((myJson) => {
+                    console.log(myJson._embedded.content);
+                    this.recipies = myJson._embedded.content;
+                    onRouteChanged();
+                });
         }
 
     });
@@ -27,13 +30,11 @@ document.addEventListener('alpine:init', () => {
 
 function onRouteChanged() {
     const hash = window.location.hash.replace("#", "");
-    //console.log(hash);
     var recipieData = Alpine.store('recipieData')
     var currentRecipie = recipieData.recipies.find(({ _url }) => _url == hash);
-    //console.log(recipieData.recipies);
-    console.log(currentRecipie);
-    recipieData.currentRecipie = currentRecipie;
-    console.log("Hash changed!");
+    if (currentRecipie !== undefined) {
+        recipieData.currentRecipie = currentRecipie;
+    }
 }
 
 window.addEventListener("hashchange", onRouteChanged);
