6b005fde4054710c3c1acc352bf09b1232c8ebfd
mikkelhm <mikkel@h-madsen.dk>
2021-07-21 14:33:29 +0200

    fix routing

--- src/mikkelhm-food/js/main.js
+++ src/mikkelhm-food/js/main.js
@@ -1,10 +1,11 @@
 var allRecipies = "https://cdn.umbraco.io/content/35339975-4e6b-40f1-8bde-23ffdee4a743/descendants?contentType=recipe";
 
-function recipieData() {
-    return {
+document.addEventListener('alpine:init', () => {
+    Alpine.store('recipieData', {
         currentRecipie: null,
         recipies: [],
-        loadRecipies() {
+        init() {
+            console.log("now");
             fetch(allRecipies, {
                 headers: {
                     'Accept-Language': 'da-DK',
@@ -17,8 +18,22 @@ function recipieData() {
             .then((myJson) => {
                 console.log(myJson._embedded.content);
                 this.recipies = myJson._embedded.content;
+                onRouteChanged();
             });
         }
 
-    }
+    });
+})
+
+function onRouteChanged() {
+    const hash = window.location.hash.replace("#", "");
+    //console.log(hash);
+    var recipieData = Alpine.store('recipieData')
+    var currentRecipie = recipieData.recipies.find(({ _url }) => _url == hash);
+    //console.log(recipieData.recipies);
+    console.log(currentRecipie);
+    recipieData.currentRecipie = currentRecipie;
+    console.log("Hash changed!");
 }
+
+window.addEventListener("hashchange", onRouteChanged);
